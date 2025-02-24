13a06a47dd610b0ff561f38926938e0b8bede11b
RomaneeChoiti <tmd1568@gmail.com>
2025-02-24 17:44:36 +0900

    feat/#5 sonner컴포넌트 구현

--- components/ui/sonner.tsx
+++ components/ui/sonner.tsx
@@ -5,20 +5,17 @@ import { Toaster as Sonner } from "sonner"
 
 type ToasterProps = React.ComponentProps<typeof Sonner>
 
-const Toaster = ({ ...props }: ToasterProps) => {
+const Toaster = ({ position = "top-center", ...props }: ToasterProps) => {
   const { theme = "system" } = useTheme()
 
   return (
     <Sonner
       theme={theme as ToasterProps["theme"]}
+      position={position}
       className="toaster group fixed top-0 left-1/2 transform -translate-x-1/2 z-50"
       toastOptions={{
-        classNames: {
-          toast: "group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:rounded-none group-[.toaster]:shadow-[5px_0_black,-5px_0_black,0_-5px_black,0_5px_black]",
-          actionButton:
-            "group-[.toast]:bg-primary group-[.toast]:text-primary-foreground",
-          cancelButton:
-            "group-[.toast]:bg-muted group-[.toast]:text-muted-foreground",
+        classNames: {          
+          toast: "group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:text-xl group-[.toaster]:two-step-border flex items-center justify-center text-center",
         },
       }}
       {...props}
