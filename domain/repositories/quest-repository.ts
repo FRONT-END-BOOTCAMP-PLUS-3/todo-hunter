a3a2d64c02ddced11ced326ab762a008102c276d
goldJeong <manage06@naver.com>
2025-02-25 16:25:02 +0900

    feat/#26 Quest interface repository 구현
    - Prisma의 Quest 모델을 기반으로 QuestRepository 인터페이스 정의
    - findById(id: number): 특정 ID의 퀘스트 조회 메서드 추가
    - findAll(): 모든 퀘스트 조회 메서드 추가
    - create : 새로운 퀘스트 생성 메서드 추가
    - update : 특정 퀘스트 업데이트 메서드 추가
    - delete 메서드는 추후 구현 고려

---
+++ domain/repositories/quest-repository.ts
@@ -0,0 +1,9 @@
+import { Quest } from "@prisma/client";
+
+export interface QuestRepository {
+    findById(id: number): Promise<Quest>;
+    findAll(): Promise<Quest[]>;
+    create(quest: Quest): Promise<Quest>;
+    update(id: number, quest: Partial<Quest>): Promise<Quest>;
+    // delete(id: number): Promise<void>;
+    }
