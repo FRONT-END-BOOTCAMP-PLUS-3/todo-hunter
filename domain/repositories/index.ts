e9aa6ce67c0cc129af866e66302eaa112a7e22af
sakedon2151 <sakedon2151@gmail.com>
2025-02-26 09:27:18 +0900

    feat/#28 export index 최적화

--- domain/repositories/index.ts
+++ domain/repositories/index.ts
@@ -3,3 +3,5 @@ export * from "@/domain/repositories/ICharacterRepository";
 export * from "@/domain/repositories/IUserTitleRepository";
 export * from "@/domain/repositories/ISuccessDayRepository";
 export * from "@/domain/repositories/IStatusRepository";
+
+export * from "@/domain/repositories/title-repository";
