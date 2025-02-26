956d485a4425ad4725540894fb57acc2d9c63949
JoostLean <jusang2@gmail.com>
2025-02-26 11:19:08 +0900

    style/#29 파일 내 일부 공백 수정

--- domain/repositories/index.ts
+++ domain/repositories/index.ts
@@ -1,5 +1,4 @@
 export * from "@/domain/repositories/IUserRepository";
 export * from "@/domain/repositories/IUserTitleRepository";
-
 export * from "@/domain/repositories/ISuccessDayRepository";
 export * from "@/domain/repositories/IStatusRepository";
