547c47c336b909e8ca771039b389cbe64b52556d
goldJeong <manage06@naver.com>
2025-02-26 13:47:36 +0900

    refactor/#30 퀘스트 ORM 구성 코드리뷰 반영
    - findAll() 삭제
    - 주간퀘스트 여부, 종료일, 태그값, 생성일 기준 조회 쿼리 추가
    - hard delete 쿼리 추가

--- domain/repositories/IQuestRepository.ts
+++ domain/repositories/IQuestRepository.ts
@@ -1,9 +1,12 @@
 import { Quest } from "@prisma/client";
 
 export interface IQuestRepository {
-    findById(id: number): Promise<Quest | null>;
-    findAll(): Promise<Quest[]>;
-    create(quest: Quest): Promise<Quest>;
-    update(id: number, quest: Partial<Quest>): Promise<Quest>;
-    // delete(id: number): Promise<void>;
-    }
+    findById: (id: number) => Promise<Quest | null>;
+    findByTag: (tag: string) => Promise<Quest[]>;
+    findWeeklyQuests: (characterId: number) => Promise<Quest[]>;
+    findBeforeEndDate: (characterId: number, endDate: Date) => Promise<Quest[]>;
+    findByCreatedAt: (characterId: number)=> Promise<Quest[]>;
+    create: (quest: Quest) => Promise<Quest>;
+    update: (id: number, quest: Partial<Quest>) => Promise<Quest>;
+    delete: (id: number) => Promise<Quest>;
+}
--- infrastructure/repositories/PriQusetRepository.ts
+++ infrastructure/repositories/PriQusetRepository.ts
@@ -9,10 +9,44 @@ export class PriQuestRepository implements IQuestRepository {
     return await this.prisma.quest.findUnique({ where: { id } });
   }
 
-  async findAll() {
-    return await this.prisma.quest.findMany();
+  // 태그값으로 퀘스트 조회
+  async findByTag(tag: string) {
+    return await this.prisma.quest.findMany({
+      where: { tagged: tag },
+    });
+  }
+  
+// 주간 퀘스트 여부 조회
+  async findWeeklyQuests(characterId: number) {
+    return await this.prisma.quest.findMany({
+      where: { 
+        characterId: characterId,
+        isWeekly: true },
+    });
   }
 
+// 종료일 기준 퀘스트 조회
+  async findBeforeEndDate(characterId: number, endDate: Date) {
+    return await this.prisma.quest.findMany({
+      where: {
+        characterId: characterId,
+        endDate: { lte: endDate }, // lte) less than or equal: 종료일이 endDate보다 작거나 같은 퀘스트 조회
+      },
+    });
+  }
+
+  // 생성일 기준 퀘스트 조회
+  async findByCreatedAt(characterId: number) {
+    return await this.prisma.quest.findMany({
+      where: {
+        characterId,
+      },
+      orderBy: {
+        createdAt: "desc", // 최신순 정렬
+      },
+    });
+  }
+  
   async create(quest: Quest) {
     return await this.prisma.quest.create({ data: quest });
   }
@@ -22,5 +56,9 @@ export class PriQuestRepository implements IQuestRepository {
       where: { id },
       data: quest,
     });
+  }
+
+  async delete(id: number) {
+    return await this.prisma.quest.delete({ where: { id } });
   }
 }
