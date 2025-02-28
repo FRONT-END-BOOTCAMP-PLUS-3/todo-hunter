1e020160b76fa93fe287c8d6cda0fa10b320b956
JoostLean <jusang2@gmail.com>
2025-02-28 10:37:13 +0900

    rename/#50 menus 상수 명칭 변경

--- constants/menu.ts
+++ constants/menu.ts
@@ -2,12 +2,10 @@
 // 예) label: ["칭호 도감"] → label: ["칭호", "도감"]
 
 // 메뉴 정보 배열 (5개로 고정 필수)
-const menus = [
+export const MENUS = [
     { menu: "character", icon: "user", label: "캐릭터" },
     { menu: "cards", icon: "book-heart", label: ["버프", "카드"] },
     { menu: "quest", icon: "receipt", label: "퀘스트" }, // 중앙 버튼
     { menu: "titlebook", icon: "trophy", label: ["칭호", "도감"] },
     { menu: "ending", icon: "octagon-check", label: "엔딩" },
 ];
-
-export default menus;
