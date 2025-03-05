793ca9bc2f496d0487765456bbc77532b232a0ad
RomaneeChoiti <tmd1568@gmail.com>
2025-03-05 17:34:03 +0900

    feat/#84 캐릭터, 몬스터 모션 구현

--- components/quest/FightField.tsx
+++ components/quest/FightField.tsx
@@ -2,6 +2,7 @@
 
 import React from "react";
 import Image from "next/image";
+import Character from "./testCharacterM";
 
 const FightField = () => {
     return (
@@ -13,6 +14,27 @@ const FightField = () => {
           style={{ objectFit: "cover" }}
           priority
         />
+          <div className="absolute bottom-0 left-0 w-1/4">
+                <Character width={170} height={170} images={[
+                    '/images/characters/player/idle01.png',
+                    '/images/characters/player/idle02.png',
+                    '/images/characters/player/idle03.png',
+                    '/images/characters/player/idle04.png',
+                    '/images/characters/player/idle05.png',
+                    '/images/characters/player/idle06.png'
+                ]} />
+            </div>
+            <div className="absolute bottom-3 right-12 w-1/4">
+                <Character width={180} height={180} images={[
+                    '/images/characters/werewolf/werewolf-idle1.png',
+                    '/images/characters/werewolf/werewolf-idle2.png',
+                    '/images/characters/werewolf/werewolf-idle3.png',
+                    '/images/characters/werewolf/werewolf-idle4.png',
+                    '/images/characters/werewolf/werewolf-idle5.png'
+                ]} />
+            </div>
+
+
       </div>);
 };
 
