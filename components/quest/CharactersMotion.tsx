5fb9a3f018a75ab23187ec5d6d09a3822bf8a445
goldJeong <manage06@naver.com>
2025-03-06 10:34:10 +0900

    feat/#84 canvas api 기본 구성
    - 캐릭터, 몬스터 background에 배치

---
+++ components/quest/CharactersMotion.tsx
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from "react";
+import Image from "next/image";
+
+interface CharacterProps {
+  idleFrames: string[];
+  attackFrames: string[];
+  alt: string;
+  top: string;
+  left: string;
+  flip?: boolean;
+  frameRate?: number;
+  isMoving?: boolean; // 이동 상태 추가
+  isAttacking?: boolean; // 공격 상태 추가
+  onMoveComplete?: () => void; // 이동 완료 시 호출할 함수
+}
+
+const CharacterMotion: React.FC<CharacterProps> = ({
+  idleFrames,
+  attackFrames,
+  alt,
+  top,
+  left,
+  flip = false,
+  frameRate = 100,
+  isMoving = false,
+  isAttacking = false,
+  onMoveComplete,
+}) => {
+  const [currentFrame, setCurrentFrame] = useState(0);
+  const [position, setPosition] = useState({ top, left });
+
+  useEffect(() => {
+    let interval: NodeJS.Timeout;
+    const frames = isAttacking ? attackFrames : idleFrames;
+
+    if (isMoving) {
+      // 플레이어가 몬스터 위치로 이동하는 애니메이션
+      setPosition({ top: "60%", left: "65%" }); // 몬스터 근처로 이동
+      setTimeout(() => {
+        onMoveComplete?.();
+      }, 500); // 500ms 후에 공격 시작
+    }
+
+    if (isAttacking) {
+      interval = setInterval(() => {
+        setCurrentFrame((prev) => (prev + 1) % frames.length);
+      }, frameRate);
+    }
+
+    return () => clearInterval(interval);
+  }, [isAttacking, isMoving, frameRate, idleFrames.length, attackFrames.length]);
+
+  return (
+    <div
+      className="absolute cursor-pointer transition-all duration-500"
+      style={{
+        top: position.top,
+        left: position.left,
+        transform: `translate(-50%, -50%) ${flip ? "scaleX(-1)" : ""}`,
+      }}
+    >
+      <Image
+        src={isAttacking ? attackFrames[currentFrame] : idleFrames[currentFrame]}
+        alt={alt}
+        width={120}
+        height={120}
+      />
+    </div>
+  );
+};
+
+export default CharacterMotion;
--- components/quest/FightField.tsx
+++ components/quest/FightField.tsx
@@ -2,18 +2,67 @@
 
 import React from "react";
 import Image from "next/image";
+import CharacterMotion from "./CharactersMotion";
 
-const FightField = () => {
-    return (
-        <div className="relative w-auto h-[150px]">
-        <Image
-          src="/images/backgrounds/underwater-fantasy-background3.png"
-          alt="field image"
-          fill
-          style={{ objectFit: "cover" }}
-          priority
-        />
-      </div>);
+const FightField = ({ isAttacking }: { isAttacking: boolean }) => {
+  const playerIdleFrames = [
+    "/images/characters/player/idle01.png",
+    "/images/characters/player/idle02.png",
+    "/images/characters/player/idle03.png",
+    "/images/characters/player/idle04.png",
+  ];
+
+  const playerAttackFrames = [
+    "/images/characters/player/attack1.png",
+    "/images/characters/player/attack2.png",
+    "/images/characters/player/attack3.png",
+    "/images/characters/player/attack4.png",
+  ];
+
+  const werewolfIdleFrames = [
+    "/images/characters/werewolf/werewolf-idle1.png",
+    "/images/characters/werewolf/werewolf-idle2.png",
+    "/images/characters/werewolf/werewolf-idle3.png",
+  ];
+
+  const werewolfAttackFrames = [
+    "/images/characters/werewolf/werewolf-attack1.png",
+    "/images/characters/werewolf/werewolf-attack2.png",
+    "/images/characters/werewolf/werewolf-attack3.png",
+    "/images/characters/werewolf/werewolf-attack4.png",
+  ];
+
+  return (
+    <div className="relative w-auto h-[200px]">
+      <Image
+        src="/images/backgrounds/underwater-fantasy-background3.png"
+        alt="field image"
+        fill
+        style={{ objectFit: "cover" }}
+        priority
+      />
+
+      {/* 플레이어 */}
+      <CharacterMotion
+        idleFrames={playerIdleFrames}
+        attackFrames={playerAttackFrames}
+        alt="Player"
+        top="60%"
+        left="30%"
+        isAttacking={isAttacking} // 공격 상태 전달
+      />
+
+      {/* 몬스터 (웨어울프) */}
+      <CharacterMotion
+        idleFrames={werewolfIdleFrames}
+        attackFrames={werewolfAttackFrames}
+        alt="Werewolf"
+        top="60%"
+        left="70%"
+        flip={true}
+      />
+    </div>
+  );
 };
 
 export default FightField;
