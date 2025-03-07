9b75064ec18c72d71e9213d7a81d92bf31cd9455
RomaneeChoiti <tmd1568@gmail.com>
2025-03-07 11:26:39 +0900

    refactory/#87 파일정리

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,8 @@
-"use client";
-
-import characterStore from "@/utils/stores/characterStore";
-import { useEffect } from "react";
-
 export default function Home() {
-  const { progress, fetchCharacterData } = characterStore();
-
-  useEffect(() => {
-    fetchCharacterData();
-  }, [progress]);
 
   return (
     <div>
       Hello world
-      <p>{progress}</p>
     </div>
   );
 }
