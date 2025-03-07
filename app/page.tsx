3acef707899cec8552c59851b28484a3cf32a757
RomaneeChoiti <tmd1568@gmail.com>
2025-03-07 11:11:18 +0900

    feat/#87 progress에서 유저데이터도 통합

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,19 @@
+"use client";
+
+import useProgressStore from "@/utils/stores/useProgressStore";
+import { useEffect } from "react";
+
 export default function Home() {
+  const { progress, fetchUserData } = useProgressStore();
+
+  useEffect(() => {
+    fetchUserData();
+  }, [progress]);
+
   return (
- <div>
-  Hello world
- </div>
+    <div>
+      Hello world
+      <p>{progress}</p>
+    </div>
   );
 }
--- app/play/character/page.tsx
+++ app/play/character/page.tsx
@@ -4,13 +4,11 @@ import { useEffect, useState } from "react";
 import { CharacterDto } from "@/application/usecases/character/dtos";
 import Status from "@/app/play/character/_components/status";
 import "@/app/play/character/_components/character.css";
-import useProgressStore from "@/utils/stores/useProgressStore";
 import Character from "@/app/play/character/_components/character";
 
 
 export default function CharacterPage() {
     const [character, setCharacter] = useState<CharacterDto | null>(null);
-    const { progress, setProgress } = useProgressStore();
 
 
     const getCharacter = async () => {
@@ -23,7 +21,7 @@ export default function CharacterPage() {
         
         const data:CharacterDto = await res.json();
         setCharacter(data);
-        setProgress(data.progress ?? 0);
+        console.log("Fetched character progress:", data.progress);
       }catch(error){
         console.log(error);
       }
@@ -38,8 +36,8 @@ export default function CharacterPage() {
             <div className="flex flex-col items-center">
                 <p className="mb-2 mt-10 text-xl text-white">{character?.nickname}님, 오늘의 경험치에요!</p>
                 <div className="flex">
-                    <p className="mr-2 text-white text-xl">{progress}%</p>
-                    <progress className="bg-white is-rounded-progress " value={progress} max="100"></progress>
+                    <p className="mr-2 text-white text-xl">{character?.progress}%</p>
+                    <progress className="bg-white is-rounded-progress " value={character?.progress} max="100"></progress>
                 </div>
             </div>
             <Character />
