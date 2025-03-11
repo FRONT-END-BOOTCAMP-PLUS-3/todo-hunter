51ec74f47e7156a21ca5c566fa9293e8a1dfe0c0
goldJeong <manage06@naver.com>
2025-03-11 16:43:52 +0900

    fix/#115 누락된 값 추가
    - 에러페이지 error, reset 값 추가
    - forwardRef에 필요한 className 정의
    - 누락된 useEffect 배열값 추가

--- app/error.tsx
+++ app/error.tsx
@@ -4,6 +4,8 @@ import Image from "next/image";
 import ErrorIcon from "@/public/icons/Error_triangle.svg"; 
 
 export default function GlobalError({ error, reset }: { error: Error; reset: () => void }) {
+    console.error("Global Error 발생:", error); 
+
     return (
         <div className="flex flex-col justify-center items-center min-h-screen bg-black text-white text-center">
         <Image src={ErrorIcon} alt="에러 아이콘" width={100} height={100} />
@@ -12,6 +14,13 @@ export default function GlobalError({ error, reset }: { error: Error; reset: () 
         <br />
         <h3 className="text-xl">INTERNAL SERVER ERROR</h3>
         <h3 className="text-xl">이용에 불편을 드려 죄송합니다.</h3>
+        <br />
+        <button 
+           onClick={reset} 
+            className="mt-4 px-6 py-2 bg-red-500 text-white font-bold rounded-md hover:bg-red-600 transition"
+        >
+            다시 시도
+        </button>
         </div>
     );
 }
