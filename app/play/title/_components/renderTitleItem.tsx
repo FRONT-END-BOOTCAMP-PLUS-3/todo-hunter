d90011d0ab422a9383a0d1ff37b5df621e1ef006
RomaneeChoiti <tmd1568@gmail.com>
2025-05-04 15:25:07 +0900

    refactor/칭호 페이지 alt 추가

--- app/play/title/_components/renderTitleItem.tsx
+++ app/play/title/_components/renderTitleItem.tsx
@@ -9,12 +9,18 @@ type TitleItemProps = {
 const RenderTitleItem = ({ title, index }: TitleItemProps) => {
     const imageUrl = title.titleId ? `/titles/title_df.png` : `${title.img}`;
     
-            return (
-                <div key={index} className="items-center justify-center text-center">
-                    <Image className="mx-auto" src={imageUrl} alt="칭호이미지" width={100} height={100} />
-                    <p>{title.name}</p>
-                </div>
-            );
+    return (
+        <div key={index} className="items-center justify-center text-center">
+            <Image 
+                className="mx-auto" 
+                src={imageUrl} 
+                alt={`${title.name || "칭호"} 이미지`} 
+                width={100} 
+                height={100} 
+            />
+            <p>{title.name}</p>
+        </div>
+    );
 }
 
 export default RenderTitleItem;
